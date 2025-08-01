74abec4ac36bf174020a15347a89008a64e5f7e7
Md Rajuan Hossen <rajuan.official@gmail.com>
2025-08-01 23:29:55 +0600

    updated

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -140,7 +140,7 @@ const Manager = () => {
             <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
 
             {/* start container */}
-            <div className="py-3 md:mycontainer min-h-[87.9vh] ">
+            <div className="py-3 mycontainer min-h-[87.9vh] ">
                 <h1 className='font-bold text-4xl text-center'><span className="text-green-600">&lt;</span>
                     Pass<span className="text-green-600">Op/&gt;</span></h1>
                 <p className='text-green-900 text-center'>Your Own Password Manager</p>
